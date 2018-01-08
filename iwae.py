b4dec114df4fe5ba6445ebaceff6ac84f0fe9d51
lxuechen <lxuechen@gmail.com>
2018-01-07 19:38:50 -0500

    add iwae evaluation

---
+++ iwae.py
@@ -0,0 +1,24 @@
+import numpy as np
+import itertools
+import time
+import sys
+
+import torch
+from torch.autograd import Variable
+from vae import VAE
+
+
+def iwae_eval(model, loader, n_sample=100):
+    model.eval()
+    time_ = time.time()
+    elbos = []
+    for i, (batch, _) in enumerate(loader):
+        batch = Variable(batch).type(model.dtype)
+        elbo, logpx, logpz, logqz = model(batch, k=n_sample)
+        elbos.append(elbo.data[0])
+
+        sys.stderr.write("batch %d, stats %.4f\n" % (i, elbo.data[0]))
+
+    mean_ = np.mean(elbos)
+    print(mean_, 'T:', time.time()-time_)
+    return mean_
